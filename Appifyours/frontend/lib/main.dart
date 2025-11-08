ac0d34b89fc275177cf29c6b2fe89c8e999ec69f
JBBSoftech <jbbsoftech@gmail.com>
2025-11-08 16:08:28 +0530

    Update Appifyours/frontend/lib/main.dart

--- Appifyours/frontend/lib/main.dart
+++ Appifyours/frontend/lib/main.dart
@@ -324,7 +324,7 @@ class _HomePageState extends State<HomePage> {
                         const Icon(Icons.store, size: 32, color: Colors.white),
                         const SizedBox(width: 8),
                         Text(
-                          'jeeva anandhan',
+                          'priya',
                           style: const TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
